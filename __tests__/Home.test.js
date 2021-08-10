b4d2b69b42b36d842fdc3a734872951d9610f107
Acelya Vuluvan <acelyavuluvan@gmail.com>
2021-08-10 17:55:14 +0300

    test updated

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -40,29 +40,36 @@ it('should allow searching by input', () => {
   expect(data).toBeDefined();
 });
 
-it('should render results for Ertan search', () => {
-  fetchMock.mockIf(
-    'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=1&srsearch=ertan?origin=*',
-    () => {
-      return {
-        // eslint-disable-next-line quotes
-        body: `{"batchcomplete":"","continue":{"sroffset":1,"continue":"-||"},"query":{"searchinfo":{"totalhits":43},"search":[{"ns":0,"title":"Ertan Saban","pageid":52180951,"size":2767,"wordcount":213,"snippet":"ethnic <span class=\"searchmatch\">origin</span>.)&quot;<span class=\"searchmatch\">Ertan</span> Saban: Allah'\u0131n verdi\u011fi kalp her zaman dolu olmal\u0131(The heart that God gives us must always be full)&quot;. &quot;<span class=\"searchmatch\">Ertan</span> Saban Kimdir?&quot;. &quot;<span class=\"searchmatch\">Ertan</span> Saban&quot;","timestamp":"2021-04-28T12:54:37Z"}]}}`,
-        headers: {
-          'content-type': 'application/json; charset=utf-8',
-        },
-      };
-    },
-  );
-  const navigation = {navigate: jest.fn()};
-  const {getByTestId, queryAllByText, getByText} = render(
-    <Home navigation={navigation} />,
-  );
-  const input = getByTestId('input');
-  const button = getByText('Search');
-  fireEvent.changeText(input, 'Ertan');
-  fireEvent.press(button);
-  const data = queryAllByText(
-    'The heart that God gives us must always be full',
-  );
-  expect(data).toBeDefined();
+const dataCheck = () => console.log('Checking Data....');
+
+describe('Checking Data', () => {
+  beforeEach(() => dataCheck());
+
+  it('should render results for Ertan search and navigate to the Results page', () => {
+    fetchMock.mockIf(
+      'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=1&srsearch=ertan?origin=*',
+      () => {
+        return {
+          // eslint-disable-next-line quotes
+          body: `{"batchcomplete":"","continue":{"sroffset":1,"continue":"-||"},"query":{"searchinfo":{"totalhits":43},"search":[{"ns":0,"title":"Ertan Saban","pageid":52180951,"size":2767,"wordcount":213,"snippet":"ethnic <span class=\"searchmatch\">origin</span>.)&quot;<span class=\"searchmatch\">Ertan</span> Saban: Allah'\u0131n verdi\u011fi kalp her zaman dolu olmal\u0131(The heart that God gives us must always be full)&quot;. &quot;<span class=\"searchmatch\">Ertan</span> Saban Kimdir?&quot;. &quot;<span class=\"searchmatch\">Ertan</span> Saban&quot;","timestamp":"2021-04-28T12:54:37Z"}]}}`,
+          headers: {
+            'content-type': 'application/json; charset=utf-8',
+          },
+        };
+      },
+    );
+    const navigation = {navigate: jest.fn()};
+    const {getByTestId, queryAllByText, getByText} = render(
+      <Home navigation={navigation} />,
+    );
+    const input = getByTestId('input');
+    const button = getByText('Search');
+    fireEvent.changeText(input, 'Ertan');
+    fireEvent.press(button);
+    const data = queryAllByText(
+      'The heart that God gives us must always be full',
+    );
+    const spy = jest.spyOn(navigation, 'navigate');
+    expect(spy).toHaveBeenCalledWith('Results', {data: data});
+  });
 });
