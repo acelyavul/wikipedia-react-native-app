a7400a226a8a665e4174cf50be247c1da2f9c0c1
Acelya Vuluvan <acelyavuluvan@gmail.com>
2022-04-10 00:35:56 +0300

    test is revised

--- __tests__/Home.test.js
+++ __tests__/Home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react-native';
+import {render, fireEvent, waitFor} from '@testing-library/react-native';
 import {enableMocks, enableFetchMocks} from 'jest-fetch-mock';
 import fetchMock from 'jest-fetch-mock';
 import Home from '../src/screens/Home';
@@ -8,6 +8,14 @@ enableFetchMocks();
 enableMocks();
 
 describe('Home screen', () => {
+  beforeEach(() => {
+    jest.useFakeTimers('legacy');
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
   it('should render default elements', () => {
     const {getByText, getByPlaceholderText} = render(<Home />);
     expect(getByText('WikiSearch')).toBeDefined();
@@ -22,37 +30,54 @@ describe('Home screen', () => {
     expect(getByPlaceholderText('Type here...').props.value).toBe('acelya');
   });
 
-  describe('Checking Data', () => {
-    it('should render results for Ertan search and navigate to the Results page', async () => {
-      fetchMock.mockIf(
-        'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=1&srsearch=ertan?origin=*',
-        () => {
-          return {
-            body: `{"batchcomplete":"","continue":{"sroffset":1,"continue":"-||"},"query":{"searchinfo":{"totalhits":43},"search":[{"ns":0,"title":"Ertan Saban","pageid":52180951,"size":2767,"wordcount":213,"snippet":"ethnic <span class="searchmatch">origin</span>.)&quot;<span class="searchmatch">Ertan</span> Saban: Allah'\u0131n verdi\u011fi kalp her zaman dolu olmal\u0131(The heart that God gives us must always be full)&quot;. &quot;<span class="searchmatch">Ertan</span> Saban Kimdir?&quot;. &quot;<span class="searchmatch">Ertan</span> Saban&quot;","timestamp":"2021-04-28T12:54:37Z"}]}}`,
-            headers: {
-              'content-type': 'application/json; charset=utf-8',
-            },
-          };
-        },
-      );
+  it('should render results for Ertan search and navigate to the Results page', async () => {
+    fetchMock.mockIf(
+      'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=1&srsearch=ertan?origin=*',
+      () => {
+        return {
+          body: `{"batchcomplete":"","continue":{"sroffset":1,"continue":"-||"},"query":{"searchinfo":{"totalhits":43},"search":[{"ns":0,"title":"Ertan Saban","pageid":52180951,"size":2767,"wordcount":213,"snippet":"ethnic <span class="searchmatch">origin</span>.)&quot;<span class="searchmatch">Ertan</span> Saban: Allah'\u0131n verdi\u011fi kalp her zaman dolu olmal\u0131(The heart that God gives us must always be full)&quot;. &quot;<span class="searchmatch">Ertan</span> Saban Kimdir?&quot;. &quot;<span class="searchmatch">Ertan</span> Saban&quot;","timestamp":"2021-04-28T12:54:37Z"}]}}`,
+          headers: {
+            'content-type': 'application/json; charset=utf-8',
+          },
+        };
+      },
+    );
 
-      const navigation = {navigate: jest.fn()};
-      const spy = jest.spyOn(navigation, 'navigate');
+    const {getByPlaceholderText, getByText, queryAllByText} = render(<Home />);
 
-      const {getByPlaceholderText, getByText, queryAllByText} = render(
-        <Home navigation={navigation} />,
-      );
+    fireEvent.changeText(getByPlaceholderText('Type here...'), 'ertan');
+    fireEvent.press(getByText('Search'));
 
-      fireEvent.changeText(getByPlaceholderText('Type here...'), 'ertan');
-      await fireEvent.press(getByText('Search'));
+    expect(queryAllByText('The heart that God gives us')).toBeDefined();
+    expect(fetch).toHaveBeenCalledWith(
+      'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=20&srsearch=ertan',
+    );
+  });
 
-      const result = queryAllByText('The heart that God gives us');
-      expect(result).toBeDefined();
-
-      expect(fetch).toHaveBeenCalledWith(
-        'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=20&srsearch=ertan',
-      );
-      expect(spy).toHaveBeenCalledTimes(1);
+  it('should display alert message when failed to load data', async () => {
+    jest.spyOn(window, 'fetch');
+    window.fetch.mockResolvedValueOnce({
+      ok: false,
+      json: async () =>
+        new Error('Failed to load... Please check your network connection!'),
     });
+
+    const screen = render(<Home />);
+    fireEvent.changeText(
+      screen.getByPlaceholderText('Type here...'),
+      'javascript',
+    );
+    fireEvent.press(screen.getByText('Search'));
+
+    await waitFor(() =>
+      expect(
+        screen.queryByText(
+          'Failed to load... Please check your network connection!',
+        ),
+      ).toBeDefined(),
+    );
+
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    window.fetch.mockRestore();
   });
 });
--- __tests__/Results.test.js
+++ __tests__/Results.test.js
@@ -3,9 +3,11 @@ import {render} from '@testing-library/react-native';
 import Results from '../src/screens/Results';
 
 describe('Results screen', () => {
+  const mockResult = 'JS, is a programming language';
+
   it('should render default elements', () => {
-    const params = {params: jest.fn()};
-    const {getByTestId} = render(<Results route={params} />);
-    expect(getByTestId('list')).toBeDefined();
+    const params = {params: mockResult};
+    const screen = render(<Results route={params} />);
+    expect(screen.getByTestId('list')).toBeDefined();
   });
 });
